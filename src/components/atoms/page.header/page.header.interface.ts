752f810771b097419f1b7582834395aa9023fff5
Carlos Bucheli <cabp@carlos-bucheli.com>
2023-11-09 15:15:18 -0500

    [001] Header Component
    Signed-off-by: Carlos Bucheli <cabp@carlos-bucheli.com>

---
+++ src/components/atoms/page.header/page.header.interface.ts
@@ -0,0 +1,4 @@
+export interface PageHeaderInterface {
+  title: string;
+  paragraphs: string[];
+}
---
+++ src/components/atoms/page.header/page.header.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import { PageHeaderInterface } from './page.header.interface';
+
+export const PageHeader: React.FC<PageHeaderInterface> = ({ title, paragraphs }: PageHeaderInterface) => {
+  /**
+   * Render paragraphs
+   *
+   * @return ReactNode[]
+   */
+  const renderParagraphs = (): ReactNode[] => {
+    const nodes: ReactNode[] = [];
+
+    paragraphs.forEach((paragraph, index) => {
+      nodes.push(<p key={ `paragraph_${ index }` } className={ index === 0 ? 'fw-bold' : '' }>{ paragraph }</p>);
+    });
+
+    return nodes;
+  };
+
+  return <div className="text-center mt-5">
+    <h4>{ title }</h4>
+    { renderParagraphs() }
+  </div>;
+};
